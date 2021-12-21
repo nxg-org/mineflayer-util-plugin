855a0c9638f9d31dd0d3e0578359be6d4b0fb1c8
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-12-20 20:30:03 -0500

    another intercept update

--- src/calcs/intercept.ts
+++ src/calcs/intercept.ts
@@ -2,29 +2,22 @@ import { Bot } from "mineflayer";
 import { Block } from "prismarine-block";
 import { Vec3 } from "vec3";
 
+type Iteration = { x: number; y: number; z: number; face: number };
+type BlockAndIterations = { block: Block | null; iterations: Iteration[] };
 export class InterceptFunctions {
     constructor(public bot: Bot) {}
 
     //Old ver grabs range between the two + direction from... from to... to.
     //calls raycast.
-    check(from: Vec3, to: Vec3) {
+    check(from: Vec3, to: Vec3): BlockAndIterations {
         //old code:
         const range = from.distanceTo(to);
         const direction = to.minus(from).normalize();
         if (isNaN(range)) return { block: null, iterations: [] };
         return this.raycast(from, direction, range);
     }
-    raycast(
-        from: Vec3,
-        direction: Vec3,
-        range: number
-    ): { block: Block | null; iterations: { x: number; y: number; z: number; face: number }[] } {
-        const iterations: {
-            x: number;
-            y: number;
-            z: number;
-            face: number;
-        }[] = [];
+    raycast(from: Vec3, direction: Vec3, range: number): BlockAndIterations {
+        const iterations: Iteration[] = [];
         const iter = new RaycastIterator(from, direction, range);
         let pos = iter.next();
         while (pos) {
